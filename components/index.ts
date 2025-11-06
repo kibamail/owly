0bc97e146c3fda0f68229b0c505777dc9477bb88
Frantz Kati <hey@katifrantz.com>
2025-11-06 21:53:47 +0100

    chore: tag v1.0.41

--- components/index.ts
+++ components/index.ts
@@ -1,25 +1,27 @@
-export * from "./alert/index.js";
+// Simple components - export everything
 export * from "./badge/index.js";
 export * from "./button/index.js";
 export * from "./calendar/index.js";
 export * from "./checkbox/index.js";
-export * from "./code-input/index.js";
-export * from "./command-search/index.js";
-export * from "./dashboard-layout/index.js";
-export * from "./dialog/index.js";
-export * from "./filter/index.js";
 export * from "./heading/index.js";
 export * from "./input-hint/index.js";
 export * from "./input-label/index.js";
 export * from "./letter-avatar/index.js";
-export * from "./multi-select/index.js";
 export * from "./progress/index.js";
-export * from "./radio-group/index.js";
-export * from "./scrollarea/index.js";
-export * from "./select-field/index.js";
 export * from "./spinner/index.js";
-export * from "./table/index.js";
-export * from "./tabs/index.js";
 export * from "./text/index.js";
-export * from "./text-field/index.js";
-export * from "./user-dropdown/index.js";
+
+// Compound components - export as namespace to avoid conflicts
+export * as Alert from "./alert/index.js";
+export * as CodeInput from "./code-input/index.js";
+export * as CommandSearch from "./command-search/index.js";
+export * as DashboardLayout from "./dashboard-layout/index.js";
+export * as Dialog from "./dialog/index.js";
+export * as Filter from "./filter/index.js";
+export * as MultiSelect from "./multi-select/index.js";
+export * as RadioGroup from "./radio-group/index.js";
+export * as SelectField from "./select-field/index.js";
+export * as Table from "./table/index.js";
+export * as Tabs from "./tabs/index.js";
+export * as TextField from "./text-field/index.js";
+export * as UserDropdown from "./user-dropdown/index.js";
